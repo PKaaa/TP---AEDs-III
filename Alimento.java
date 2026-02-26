cdd0c3c4c58f995b87f092ee615ebd3efbaf58f6
Paula Oliveira <127148342+PKaaa@users.noreply.github.com>
2026-02-26 07:27:28 -0300

    Add files via upload

---
+++ Alimento.java
@@ -0,0 +1,67 @@
+package TP;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Alimento {
+     private int id;
+     private String nome;
+     private String[] categoria;
+
+     public Alimento() {
+          this.id = 0;
+          this.nome = "";
+          this.categoria = new String[0];
+     }
+
+     public Alimento (int id, String nome, String[] categoria) {
+          this.id = id;
+          this.nome = nome;
+          this.categoria = categoria;
+     }
+
+     public Alimento (String nome, String[] categoria) {
+          this.nome = nome;
+          this.categoria = categoria;
+     }
+
+     //getters e setters
+     public int getId() {return id;}
+     public String getNome() {return nome;}
+     public String[] getCategoria() {return categoria;}
+
+     public void setId (int id) {this.id = id;}
+     public void setNome (String nome) {this.nome = nome;}
+     public void setCategoria (String[] categoria) {this.categoria = categoria;}
+
+     @Override
+     public String toString() {
+          return "Alimento {id = " + id + 
+          "; Nome = " + nome + 
+          "; Categoria(s) = " + String.join(", ", categoria) +
+          "}";
+     }
+
+     @Override
+     public int hashCode() {return id;}
+
+     public byte[] toByteArray() throws IOException {
+          ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          DataOutputStream dos = new DataOutputStream(baos);
+
+          dos.writeInt(id);
+          dos.writeUTF(nome);
+          dos.writeInt(categoria.length);
+          for (String cat : categoria) {
+               dos.writeUTF(cat);
+          }
+
+          return baos.toByteArray();
+
+     }
+
+     public void fromByteArray(byte[] b) throws IOException {
+          throw new UnsupportedOperationException ("Unimplemented method 'fromByteArray'");
+     }
+}
---
+++ AlimentoDAO.java
@@ -0,0 +1,29 @@
+package TP;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class AlimentoDAO {
+     private Arquivo <Alimento> arq;
+
+     //construtor
+     public AlimentoDAO () throws Exception {
+          arq = new Arquivo <> ("alimentos", Alimento.class.getConstructor());
+     }
+
+     public boolean incluirAlimento (Alimento a) throws Exception {
+          return arq.create(a) > 0;
+     }
+
+     public Alimento buscarAlimento (int id) throws Exception {
+          return arq.read(id);
+     }
+
+     public boolean alterarAlimento (Alimento a) throws Exception {
+          return arq.update(a);
+     }
+
+     public boolean excluirAlimento (int id) throws Exception {
+          return arq.delete(id);
+     }
+}
