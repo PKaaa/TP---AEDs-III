00c323d29194ab9fa942b3222e40304e2dc73012
Paula Oliveira <127148342+PKaaa@users.noreply.github.com>
2026-02-28 19:34:53 -0300

    Add search methods for Alimento by name and category

--- AlimentoDAO.java
+++ AlimentoDAO.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatter; //nao eh usado nenhuma dessas bibliotecas POR ENQUANTO
 
 public class AlimentoDAO {
      private Arquivo <Alimento> arq;
@@ -13,8 +13,28 @@ public class AlimentoDAO {
           return arq.create(a) > 0;
      }
 
-     public Alimento buscarAlimento (int id) throws Exception {
+     public Alimento buscarAlimentoID (int id) throws Exception {
           return arq.read(id);
+     }
+
+     public Alimento buscarAlimentoNome (String nome) throws Exception {
+          for (Alimento a : arq.readAll()) {
+               if (a.getNome().equalsIgnoreCase(nome)) {
+                    return a;
+               }
+          }
+          return null;
+     }
+
+     public Alimento buscarAlimentoCategoria (String categoria) throws Exception {
+          for (Alimento a : arq.readAll()) {
+               for (String cat : a.getCategoria()) {
+                    if (cat.equalsIgnoreCase(categoria)) {
+                         return a;
+                    }
+               }
+          }
+          return null;
      }
 
      public boolean alterarAlimento (Alimento a) throws Exception {
@@ -25,4 +45,3 @@ public class AlimentoDAO {
           return arq.delete(id);
      }
 }
-
