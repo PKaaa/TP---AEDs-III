51d2f0df7ea8861e376da6e0420a29171cc5a26c
Paula Oliveira <127148342+PKaaa@users.noreply.github.com>
2026-02-28 19:36:26 -0300

    Add files via upload

---
+++ MenuAlimento.java
@@ -0,0 +1,158 @@
+package TP;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class MenuAlimento {
+     private AlimentoDAO alimentoDAO;
+     private Scanner console = new Scanner (System.in);
+     
+     public MenuAlimento () throws Exception {
+          alimentoDAO = new AlimentoDAO();
+     }
+
+     void menu() throws Exception {
+          int op;
+
+          do {
+               System.out.println ("\n\nInicio > Alimentos");
+               System.out.println ("\n\n1 - Adicionar Alimento");
+               System.out.println ("\n\n2 - Buscar Alimento");
+               System.out.println ("\n\n3 - Alterar Alimento");
+               System.out.println ("\n\n4 - Excluir Alimento");
+               System.out.println ("\n\n0 - Voltar/Sair");
+
+               try {
+                    op = Integer.valueOf(console.nextInt());
+               } catch (NumberFormatException e) {
+                    op = -1;//erro
+               }
+
+               switch (op) {
+                    case 1:
+                         incluirAlimento();
+                         break;
+                    case 2:
+                         buscarAlimento();
+                         break;
+                    case 3:
+                         alterarAlimento();
+                         break;
+                    case 4:
+                         excluirAlimento();
+                         break;
+                    default:
+                         System.out.println ("\nOpção inválida!");
+                         break;
+               }
+          } while (op != 0);
+     }
+     
+     private void buscarAlimento() throws Exception {
+          System.out.println ("\n\nDigite por qual meio voce gostaria de buscar o alimento: ");
+          System.out.println ("\n1 - ID");
+          System.out.println ("\n2 - Nome");
+          System.out.println ("\n3 - Categoria");
+          int op = console.nextInt();
+          console.nextLine();
+
+          Alimento a;
+          
+          try {
+               switch (op) {
+                    case 1:
+                         System.out.println ("\n\nDigite o ID do alimento: ");
+                         int id = console.nextInt();
+                         console.nextLine();
+                         a = alimentoDAO.buscarAlimentoID(id);
+                         if (a != null) {
+                              System.out.println (a);
+                         } else {
+                              System.out.println("\nAlimento nao encontrado.");
+                         }
+                         break;
+                    
+                    case 2:
+                         System.out.println ("\n\nDigite o nome do alimento: ");
+                         String nome = console.nextLine();
+                         a = alimentoDAO.buscarAlimentoNome(nome);
+
+                         if (a != null) {
+                              System.out.println (a);
+                         } else {
+                              System.out.println ("\nAlimento nao encontrado.");
+                         }
+                         break;
+
+                    case 3:
+                         System.out.println ("\n\nDigite a categoria do alimento: ");
+                         String categoria = console.nextLine();
+                         a = alimentoDAO.buscarAlimentoCategoria(categoria);
+
+                         if (a != null) {
+                              System.out.println (a);
+                         } else {
+                              System.out.println ("\nAlimento nao encontrado.");
+                         }
+                         break;
+
+                    default:
+                         System.out.println ("\nOpcao invalida!");
+                         break;
+               }
+          } catch (NumberFormatException e) {
+               op = -1;//erro
+          }
+     }
+
+     private void incluirAlimento() throws Exception {
+          System.out.println ("\n\nDigite o nome do alimento: ");
+          String nome = console.nextLine();
+          System.out.println ("\n\nDigite as categorias do alimento (separadamente por virgula): ");
+          String[] categorias = console.nextLine().split(",");
+          Alimento a = new Alimento(0, nome, categorias);
+          if (alimentoDAO.incluirAlimento(a)) {
+               System.out.println ("\nAlimento incluido com sucesso!");
+          } else {
+               System.out.println ("\nErro ao incluir alimento.");
+          }
+     }
+
+     private void alterarAlimento() throws Exception {
+          System.out.println ("\n\nDigite o ID do alimento: ");
+          int id = console.nextInt();
+          console.nextLine();
+
+          Alimento a = alimentoDAO.buscarAlimentoID(id);
+
+          if (a != null) {
+               System.out.println ("\n\nDigite o nome do alimento: ");
+               String nome = console.nextLine();
+               System.out.println ("\n\nDigite as categorias do alimento (separadamente por virgula): ");
+               String[] categorias = console.nextLine().split(",");
+               a.setNome(nome);
+               a.setCategoria(categorias);
+
+               if (alimentoDAO.alterarAlimento(a)) {
+                    System.out.println ("\nAlimento alterado com sucesso!");
+               } else {
+                    System.out.println ("\nErro ao alterar alimento.");
+               }
+          } else {
+               System.out.println ("\nAlimento nao encontrado.");
+          }
+     }
+
+     private void excluirAlimento() throws Exception {
+          System.out.println ("\n\nDigite o ID do alimento: ");
+          int id = console.nextInt();
+          console.nextLine();
+
+          if (alimentoDAO.excluirAlimento(id)) {
+               System.out.println ("\nAlimento excluido com sucesso!");
+          } else {
+               System.out.println ("\nErro ao excluir alimento.");
+          }
+     }
+}
---
+++ MenuReceitas.java
@@ -0,0 +1,205 @@
+package TP;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class MenuReceitas {
+     private ReceitaDAO receitaDAO;
+     private Scanner console = new Scanner (System.in);
+
+     public MenuReceitas() throws Exception {
+          receitaDAO = new ReceitaDAO();
+     }
+
+     public void menu() throws Exception {
+          int op;
+
+          do {
+               System.out.println ("\n\nInicio > Receitas");
+               System.out.println ("\n\n1 - Adicionar Receita");
+               System.out.println ("\n\n2- Buscar Receita");
+               System.out.println ("\n\n3 - Alterar Receita");
+               System.out.println ("\n\n4 - Excluir Receita");
+               System.out.println ("\n\n0 - Voltar/Sair");
+
+               try {
+                    op = Integer.valueOf(console.nextInt());
+               } catch (NumberFormatException e) {
+                    op = -1;//erro
+               }
+
+               switch (op) {
+                    case 1:
+                         incluirReceita();
+                         break;
+                    case 2:
+                         buscarReceita();
+                         break;
+                    case 3:
+                         alterarReceita();
+                         break;
+                    case 4:
+                         excluirReceita();
+                         break;
+                    default:
+                         System.out.println ("\nOpção inválida!");
+                         break;
+               }
+          } while (op != 0);
+     }
+
+     private void buscarReceita() throws Exception {
+          System.out.println ("\n\nDigite por qual meio voce gostaria de buscar a receita: ");
+          System.out.println ("\n1 - ID");
+          System.out.println ("\n2 - Titulo");
+          System.out.println ("\n3 - Tempo de preparo");
+          int op = console.nextInt();
+          console.nextLine();
+
+          Receita r;
+
+          try {
+               switch (op) {
+                    case 1:
+                         System.out.println ("\n\nDigite o ID da receita: ");
+                         int id = console.nextInt();
+                         console.nextLine();
+                         r = receitaDAO.buscarReceitaID(id);
+                         if (r != null) {
+                              System.out.println (r);
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+
+                    case 2:
+                         System.out.println ("\n\nDigite o nome da receita: ");
+                         String nome = console.nextLine();
+                         r = receitaDAO.buscarReceitaTitulo(nome);
+                         if (r != null) {
+                              System.out.println (r);
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+                    
+                    case 3:
+                         System.out.println ("\n\nDigite o tempo de preparo da receita: ");
+                         int tempo = console.nextInt();
+                         console.nextLine();
+                         r = receitaDAO.buscarReceitaTempo(tempo);
+                         if (r != null) {
+                              System.out.println (r);
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+
+                    default:
+                         System.out.println ("\nOpção inválida!");
+                         break;
+               }
+          } catch (NumberFormatException e) {
+               System.out.println ("\nErro ao buscar receita.");
+          }
+     }
+
+     private void incluirReceita() throws Exception {
+          System.out.println ("\n\nDigite o nome da receita: ");
+          String nome = console.nextLine();
+          System.out.println ("\n\nDigite as informações da receita: ");
+          String informacoes = console.nextLine();
+          System.out.println ("\n\nDigite o tempo de preparo da receita (em minutos): ");
+          int tempo = console.nextInt();
+          console.nextLine();
+          System.out.println ("\n\nDigite a porção da receita: ");
+          String porcao = console.nextLine();
+          Receita r = new Receita(nome, informacoes, tempo, porcao);
+          if (receitaDAO.incluirReceita(r)) {
+               System.out.println ("\nReceita incluída com sucesso!");
+          } else {
+               System.out.println ("\nErro ao incluir receita.");
+          }
+     }
+
+     private void alterarReceita() throws Exception {
+          System.out.println ("\n\nDigite o que voce gostaria de alterar: ");
+          System.out.println ("\n1 - Titulo");
+          System.out.println ("\n2 - Informações");
+          System.out.println ("\n3 - Tempo de preparo");
+          System.out.println ("\n4 - Porção");
+          int op = console.nextInt();
+          console.nextLine();
+
+          System.out.println ("\n\nDigite o ID da receita que voce gostaria de alterar: ");
+          int id = console.nextInt();
+          console.nextLine();
+          try {
+               switch (op) {
+                    case 1:
+                         System.out.println ("\n\nDigite o novo titulo da receita: ");
+                         String nome = console.nextLine();
+                         Receita r = receitaDAO.buscarReceitaID(id);
+                         if (r != null) {
+                              r.setTitulo(nome);
+                              if (receitaDAO.alterarReceita(r)) {
+                                   System.out.println ("\nReceita alterada com sucesso!");
+                              } else {
+                                   System.out.println ("\nErro ao alterar receita.");
+                              }
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+                    case 2:
+                         System.out.println ("\n\nDigite as novas informações da receita: ");
+                         String informacoes = console.nextLine();
+                         r = receitaDAO.buscarReceitaID(id);
+                         if (r != null) {
+                              r.setInformacoes(informacoes);
+                              if (receitaDAO.alterarReceita(r)) {
+                                   System.out.println ("\nReceita alterada com sucesso!");
+                              } else {
+                                   System.out.println ("\nErro ao alterar receita.");
+                              }
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+                    case 3:
+                         System.out.println ("\n\nDigite o novo tempo de preparo da receita (em minutos): ");
+                         int tempo = console.nextInt();
+                         console.nextLine();
+                         r = receitaDAO.buscarReceitaID(id);
+                         if (r != null) {
+                              r.setTempoPreparo(tempo);
+                              if (receitaDAO.alterarReceita(r)) {
+                                   System.out.println ("\nReceita alterada com sucesso!");
+                              } else {
+                                   System.out.println ("\nErro ao alterar receita.");
+                              }
+                         } else {
+                              System.out.println("\nReceita nao encontrada.");
+                         }
+                         break;
+                    default:
+                         System.out.println ("\nOpção inválida!");
+                         break;
+               }
+          } catch (NumberFormatException e) {
+               System.out.println ("\nErro ao alterar receita.");
+          }
+     }
+
+     private void excluirReceita() throws Exception {
+          System.out.println ("\n\nDigite o ID da receita que voce gostaria de excluir: ");
+          int id = console.nextInt();
+          console.nextLine();
+          if (receitaDAO.excluirReceita(id)) {
+               System.out.println ("\nReceita excluída com sucesso!");
+          } else {
+               System.out.println ("\nErro ao excluir receita.");
+          }
+     }
+}
