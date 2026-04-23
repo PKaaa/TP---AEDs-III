27da3710c57ab8ef944c374697c37de3fb1c06d5
Marina Campidelli <123119193+MCampidelli@users.noreply.github.com>
2026-04-22 22:23:45 -0300

    Refactor AlimentoDAO to improve search methods
    Updated methods to handle null checks and prevent duplicates.

--- src/dao/AlimentoDAO.java
+++ src/dao/AlimentoDAO.java
@@ -3,47 +3,61 @@ package dao;
 import model.Alimento;
 import util.Arquivo;
 
+import java.util.ArrayList;
+
 public class AlimentoDAO {
-     private Arquivo <Alimento> arq;
+    private Arquivo<Alimento> arq;
 
-     //construtor
-     public AlimentoDAO () throws Exception {
-          arq = new Arquivo <> ("alimentos", Alimento.class.getConstructor());
-     }
+    public AlimentoDAO() throws Exception {
+        arq = new Arquivo<>("alimentos", Alimento.class.getConstructor());
+    }
 
-     public boolean incluirAlimento (Alimento a) throws Exception {
-          return arq.create(a) > 0;
-     }
+    // CREATE
+    public boolean incluirAlimento(Alimento a) throws Exception {
+        return arq.create(a) > 0;
+    }
 
-     public Alimento buscarAlimentoID (int id) throws Exception {
-          return arq.read(id);
-     }
+    // READ por ID 
+    public Alimento buscarAlimentoID(int id) throws Exception {
+        return arq.read(id);
+    }
 
-     public Alimento buscarAlimentoNome (String nome) throws Exception {
-          for (Alimento a : arq.readAll()) {
-               if (a.getNome().equalsIgnoreCase(nome)) {
-                    return a;
-               }
-          }
-          return null;
-     }
+    // READ por nome
+    public Alimento buscarAlimentoNome(String nome) throws Exception {
+        for (Alimento a : arq.readAll()) {
+            if (a.getNome() != null && a.getNome().equalsIgnoreCase(nome)) {
+                return a;
+            }
+        }
+        return null;
+    }
 
-     public Alimento buscarAlimentoCategoria (String categoria) throws Exception {
-          for (Alimento a : arq.readAll()) {
-               for (String cat : a.getCategoria()) {
-                    if (cat.equalsIgnoreCase(categoria)) {
-                         return a;
+    // READ por categoria
+    public ArrayList<Alimento> buscarAlimentoCategoria(String categoria) throws Exception {
+        ArrayList<Alimento> lista = new ArrayList<>();
+
+        for (Alimento a : arq.readAll()) {
+
+            String[] categorias = a.getCategoria();
+
+            if (categorias != null) {
+                for (String cat : categorias) {
+                    if (cat != null && cat.trim().equalsIgnoreCase(categoria.trim())) {
+                        lista.add(a);
+                        break; // evita duplicar o mesmo alimento
                     }
-               }
-          }
-          return null;
-     }
+                }
+            }
+        }
 
-     public boolean alterarAlimento (Alimento a) throws Exception {
-          return arq.update(a);
-     }
+        return lista;
+    }
 
-     public boolean excluirAlimento (int id) throws Exception {
-          return arq.delete(id);
-     }
+    public boolean alterarAlimento(Alimento a) throws Exception {
+        return arq.update(a);
+    }
+
+    public boolean excluirAlimento(int id) throws Exception {
+        return arq.delete(id);
+    }
 }
